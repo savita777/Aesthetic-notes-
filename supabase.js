302fbfe3251a191e07bf92e0dd0e0940cf5d80bb
savita777 <savitawork123@gmail.com>
2026-08-28 00:22:48 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 // 👇 Apni actual URL aur Key daalein (Dhyan rahe, single quotes '' delete na hon)
-const supabaseUrl = 'https://gcrebjfyxqdlkxpyilfc.supabase.co';
-const supabaseAnonKey = 'sb_publishable_k2v9Y3E-4YbhzDnk_QjucQ_ntRgbp95';
+const supabaseUrl = 'https://wknpkbifrxcmrpurxmjm.supabase.co';
+const supabaseAnonKey = 'sb_publishable_qXdJaio7Bm5cUoHxs6A9iQ_JLtYDr7L';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
